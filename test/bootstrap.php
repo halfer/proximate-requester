12661153e5c5704e9b96c52206c489b285b2be19
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-07 19:26:33 +0100

    Make a start on some unit tests

---
+++ test/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Initialises app ready for testing
+ */
+
+$rootPath = realpath(__DIR__ . '/..');
+require_once $rootPath . '/vendor/autoload.php';
---
+++ test/unit/tests/FilesystemTest.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Unit tests for the CacheAdapter\Filesystem class
+ */
+
+use PHPUnit\Framework\TestCase;
+use League\Flysystem\Filesystem as FlysystemAdapter;
+use Proximate\CacheAdapter\Filesystem;
+use Cache\Adapter\Filesystem\FilesystemCachePool;
+
+class FilesystemTest extends TestCase
+{
+    protected $flysystem;
+
+    /**
+     * Create a mocked flysystem for all tests
+     */
+    public function setUp()
+    {
+        $this->flysystem = Mockery::mock(FlysystemAdapter::class);
+    }
+
+    public function testCountCacheItems()
+    {
+        $cacheKeys = $this->getSmallCacheKeySet();
+        $this->setCacheListExpectation($cacheKeys);
+        $this->assertEquals(count($cacheKeys), $this->getCacheAdapter()->countCacheItems());
+    }
+
+    public function testGetAllCacheKeys()
+    {
+        $cacheKeys = $this->getSmallCacheKeySet();
+        $this->setCacheListExpectation($cacheKeys);
+        $this->assertEquals($cacheKeys, $this->getCacheAdapter()->getPageOfCacheKeys(1, 10));
+    }
+
+    protected function getSmallCacheKeySet()
+    {
+        return [1, 2, 3, ];
+    }
+
+    /**
+     * Tests that pagination works
+     *
+     * @dataProvider paginationDataProvider
+     * @param integer $page
+     * @param array $expectedResult
+     */
+    public function testPagination($page, array $expectedResult)
+    {
+        $cacheKeys = $this->getLargeCacheKeySet();
+        $this->setCacheListExpectation($cacheKeys);
+        $this->assertEquals(
+            $expectedResult,
+            $this->getCacheAdapter()->getPageOfCacheKeys($page, 3)
+        );
+    }
+
+    public function testGetCacheItems()
+    {
+        $cacheKeys = $this->getSmallCacheKeySet();
+        $this->setCacheListExpectation($cacheKeys);
+
+        // Set up mock for cache pool
+        $cacheItems = ['Item 1', 'Item 2', 'Item 3', ];
+        $cachePool = Mockery::mock(FilesystemCachePool::class);
+        $cachePool->
+            shouldReceive('getItems')->
+            with($cacheKeys)->
+            andReturn($cacheItems);
+
+        $result = $this->
+            getCacheAdapter()->
+            getPageOfCacheItems($cachePool, 1, count($cacheItems));
+        $this->assertEquals($cacheItems, $result);
+    }
+
+    public function paginationDataProvider()
+    {
+        return [
+            [1, [1, 2, 3, ]], // Test first page
+            [2, [4, 5, 6, ]], // Test subsequent page
+            [3, [7, ]],       // Test a partial page
+        ];
+    }
+
+    protected function getLargeCacheKeySet()
+    {
+        return [1, 2, 3, 4, 5, 6, 7, ];
+    }
+
+    protected function getCacheAdapter()
+    {
+        return new Filesystem($this->getMockedFlysystem());
+    }
+
+    protected function setCacheListExpectation(array $cacheKeys)
+    {
+        $this->
+            getMockedFlysystem()->
+            shouldReceive('listContents')->
+            with('cache')->
+            andReturn($cacheKeys);
+    }
+
+    protected function getMockedFlysystem()
+    {
+        return $this->flysystem;
+    }
+}
